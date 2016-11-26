c5f31a13da1e8a682422b661e355f3bb344ed092
Paul Clifford <paulclifford2570@gmail.com>
2016-11-26 15:14:25 +0000

    start assignment 6

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@
    def main_menu
      puts "Main Menu - #{address_book.entries.count} entries"
      puts "1 - View all entries"
-     puts "2 - Create an entry"
-     puts "3 - Search for an entry"
-     puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "2 - View Entry Number n"
+     puts "3 - Create an entry"
+     puts "4 - Search for an entry"
+     puts "5 - Import entries from a CSV"
+     puts "6 - Exit"
      print "Enter your selection: "
  
      selection = gets.to_i
@@ -27,19 +28,24 @@
          main_menu
        when 2
          system "clear"
-         create_entry
+         print "Enter An Entry Number"
+         entry_number = gets.chomp
+         view_entry_number
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
-       
          exit(0)
    
        else
@@ -103,7 +109,6 @@
      # #19
        when "d"
        when "e"
-     # #20
        when "m"
          system "clear"
          main_menu
@@ -113,5 +118,11 @@
          entry_submenu(entry)
      end
    end
+   
+   def view_entry_number
+       puts "you are viewing an entry number"
+       puts "Good-bye!"
+       exit(0)
+    end
    
  end
